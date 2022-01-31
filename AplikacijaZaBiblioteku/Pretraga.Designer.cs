c3525c14104f5c9ba1d9a65ef66891abf18f8b6f
UltraXDZN <57915175+UltraXDZN@users.noreply.github.com>
2022-01-31 09:27:38 +0100

    Update Pretraga

--- AplikacijaZaBiblioteku/Pretraga.Designer.cs
+++ AplikacijaZaBiblioteku/Pretraga.Designer.cs
@@ -28,60 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.VracanjePretraga = new System.Windows.Forms.TextBox();
             this.PretragaButun = new System.Windows.Forms.Button();
-            this.KorisnikPretraga = new System.Windows.Forms.TextBox();
-            this.KnjigaPretraga = new System.Windows.Forms.TextBox();
-            this.PosudbaPretraga = new System.Windows.Forms.TextBox();
             this.PretragaRichTextbox = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.imeLabel = new System.Windows.Forms.Label();
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeLabel = new System.Windows.Forms.Label();
+            this.OIBtextBox = new System.Windows.Forms.TextBox();
+            this.OIBLabel = new System.Windows.Forms.Label();
+            this.ISBNtextBox = new System.Windows.Forms.TextBox();
+            this.ISBNLabel = new System.Windows.Forms.Label();
+            this.AutortextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.ZatvoriButun = new System.Windows.Forms.Button();
-            this.OIBCheck = new System.Windows.Forms.CheckBox();
-            this.PosudbaCheck = new System.Windows.Forms.CheckBox();
-            this.VracanjeCheck = new System.Windows.Forms.CheckBox();
-            this.ISBNCheck = new System.Windows.Forms.CheckBox();
+            this.NazivtextBox = new System.Windows.Forms.TextBox();
+            this.Nazivlabel = new System.Windows.Forms.Label();
+            this.pretragaKorisnikaButton = new System.Windows.Forms.Button();
+            this.pretragaKnjigeButton = new System.Windows.Forms.Button();
+            this.pretragaPosuditeljaKnjigeButton = new System.Windows.Forms.Button();
+            this.pretragaLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // VracanjePretraga
-            // 
-            this.VracanjePretraga.Location = new System.Drawing.Point(333, 146);
-            this.VracanjePretraga.Name = "VracanjePretraga";
-            this.VracanjePretraga.Size = new System.Drawing.Size(100, 20);
-            this.VracanjePretraga.TabIndex = 0;
             // 
             // PretragaButun
             // 
-            this.PretragaButun.Location = new System.Drawing.Point(333, 194);
+            this.PretragaButun.Location = new System.Drawing.Point(413, 194);
             this.PretragaButun.Name = "PretragaButun";
             this.PretragaButun.Size = new System.Drawing.Size(100, 23);
             this.PretragaButun.TabIndex = 2;
             this.PretragaButun.Text = "Pretraži";
             this.PretragaButun.UseVisualStyleBackColor = true;
             this.PretragaButun.Click += new System.EventHandler(this.PretragaButun_Click);
-            // 
-            // KorisnikPretraga
-            // 
-            this.KorisnikPretraga.Location = new System.Drawing.Point(333, 21);
-            this.KorisnikPretraga.Name = "KorisnikPretraga";
-            this.KorisnikPretraga.Size = new System.Drawing.Size(100, 20);
-            this.KorisnikPretraga.TabIndex = 3;
-            // 
-            // KnjigaPretraga
-            // 
-            this.KnjigaPretraga.Location = new System.Drawing.Point(333, 60);
-            this.KnjigaPretraga.Name = "KnjigaPretraga";
-            this.KnjigaPretraga.Size = new System.Drawing.Size(100, 20);
-            this.KnjigaPretraga.TabIndex = 4;
-            // 
-            // PosudbaPretraga
-            // 
-            this.PosudbaPretraga.Location = new System.Drawing.Point(333, 103);
-            this.PosudbaPretraga.Name = "PosudbaPretraga";
-            this.PosudbaPretraga.Size = new System.Drawing.Size(100, 20);
-            this.PosudbaPretraga.TabIndex = 5;
             // 
             // PretragaRichTextbox
             // 
@@ -91,108 +66,170 @@
             this.PretragaRichTextbox.TabIndex = 6;
             this.PretragaRichTextbox.Text = "";
             // 
-            // label1
+            // imeLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(330, 5);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "OIB Korisnika";
+            this.imeLabel.AutoSize = true;
+            this.imeLabel.Location = new System.Drawing.Point(330, 46);
+            this.imeLabel.Name = "imeLabel";
+            this.imeLabel.Size = new System.Drawing.Size(27, 13);
+            this.imeLabel.TabIndex = 13;
+            this.imeLabel.Text = "Ime:";
+            this.imeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(333, 62);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.imeTextBox.TabIndex = 14;
+            // 
+            // prezimeTextBox
+            // 
+            this.prezimeTextBox.Location = new System.Drawing.Point(333, 110);
+            this.prezimeTextBox.Name = "prezimeTextBox";
+            this.prezimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.prezimeTextBox.TabIndex = 16;
+            // 
+            // prezimeLabel
+            // 
+            this.prezimeLabel.AutoSize = true;
+            this.prezimeLabel.Location = new System.Drawing.Point(330, 94);
+            this.prezimeLabel.Name = "prezimeLabel";
+            this.prezimeLabel.Size = new System.Drawing.Size(47, 13);
+            this.prezimeLabel.TabIndex = 15;
+            this.prezimeLabel.Text = "Prezime:";
+            this.prezimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OIBtextBox
+            // 
+            this.OIBtextBox.Location = new System.Drawing.Point(333, 158);
+            this.OIBtextBox.Name = "OIBtextBox";
+            this.OIBtextBox.Size = new System.Drawing.Size(100, 20);
+            this.OIBtextBox.TabIndex = 18;
+            // 
+            // OIBLabel
+            // 
+            this.OIBLabel.AutoSize = true;
+            this.OIBLabel.Location = new System.Drawing.Point(330, 142);
+            this.OIBLabel.Name = "OIBLabel";
+            this.OIBLabel.Size = new System.Drawing.Size(28, 13);
+            this.OIBLabel.TabIndex = 17;
+            this.OIBLabel.Text = "OIB:";
+            this.OIBLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ISBNtextBox
+            // 
+            this.ISBNtextBox.Location = new System.Drawing.Point(481, 158);
+            this.ISBNtextBox.Name = "ISBNtextBox";
+            this.ISBNtextBox.Size = new System.Drawing.Size(100, 20);
+            this.ISBNtextBox.TabIndex = 25;
+            // 
+            // ISBNLabel
+            // 
+            this.ISBNLabel.AutoSize = true;
+            this.ISBNLabel.Location = new System.Drawing.Point(478, 142);
+            this.ISBNLabel.Name = "ISBNLabel";
+            this.ISBNLabel.Size = new System.Drawing.Size(35, 13);
+            this.ISBNLabel.TabIndex = 24;
+            this.ISBNLabel.Text = "ISBN:";
+            this.ISBNLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AutortextBox
+            // 
+            this.AutortextBox.Location = new System.Drawing.Point(481, 110);
+            this.AutortextBox.Name = "AutortextBox";
+            this.AutortextBox.Size = new System.Drawing.Size(100, 20);
+            this.AutortextBox.TabIndex = 23;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(330, 44);
+            this.label2.Location = new System.Drawing.Point(478, 94);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "ISBN Knjige";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Autor:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
+            // NazivtextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(333, 87);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Datum Posudbe";
+            this.NazivtextBox.Location = new System.Drawing.Point(481, 62);
+            this.NazivtextBox.Name = "NazivtextBox";
+            this.NazivtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NazivtextBox.TabIndex = 21;
             // 
-            // label4
+            // Nazivlabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(336, 130);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(83, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Datum Vraćanja";
+            this.Nazivlabel.AutoSize = true;
+            this.Nazivlabel.Location = new System.Drawing.Point(478, 46);
+            this.Nazivlabel.Name = "Nazivlabel";
+            this.Nazivlabel.Size = new System.Drawing.Size(37, 13);
+            this.Nazivlabel.TabIndex = 20;
+            this.Nazivlabel.Text = "Naziv:";
+            this.Nazivlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // ZatvoriButun
+            // pretragaKorisnikaButton
             // 
-            this.ZatvoriButun.Location = new System.Drawing.Point(333, 240);
-            this.ZatvoriButun.Name = "ZatvoriButun";
-            this.ZatvoriButun.Size = new System.Drawing.Size(100, 23);
-            this.ZatvoriButun.TabIndex = 11;
-            this.ZatvoriButun.Text = "Zatvori";
-            this.ZatvoriButun.UseVisualStyleBackColor = true;
-            this.ZatvoriButun.Click += new System.EventHandler(this.ZatvoriButun_Click);
+            this.pretragaKorisnikaButton.Location = new System.Drawing.Point(614, 62);
+            this.pretragaKorisnikaButton.Name = "pretragaKorisnikaButton";
+            this.pretragaKorisnikaButton.Size = new System.Drawing.Size(75, 23);
+            this.pretragaKorisnikaButton.TabIndex = 26;
+            this.pretragaKorisnikaButton.Text = "Posuditelja";
+            this.pretragaKorisnikaButton.UseVisualStyleBackColor = true;
+            this.pretragaKorisnikaButton.Click += new System.EventHandler(this.pretragaKorisnikaButton_Click);
             // 
-            // OIBCheck
+            // pretragaKnjigeButton
             // 
-            this.OIBCheck.AutoSize = true;
-            this.OIBCheck.Location = new System.Drawing.Point(311, 24);
-            this.OIBCheck.Name = "OIBCheck";
-            this.OIBCheck.Size = new System.Drawing.Size(15, 14);
-            this.OIBCheck.TabIndex = 12;
-            this.OIBCheck.UseVisualStyleBackColor = true;
+            this.pretragaKnjigeButton.Location = new System.Drawing.Point(614, 107);
+            this.pretragaKnjigeButton.Name = "pretragaKnjigeButton";
+            this.pretragaKnjigeButton.Size = new System.Drawing.Size(75, 23);
+            this.pretragaKnjigeButton.TabIndex = 27;
+            this.pretragaKnjigeButton.Text = "Knjige";
+            this.pretragaKnjigeButton.UseVisualStyleBackColor = true;
+            this.pretragaKnjigeButton.Click += new System.EventHandler(this.pretragaKnjigeButton_Click);
             // 
-            // PosudbaCheck
+            // pretragaPosuditeljaKnjigeButton
             // 
-            this.PosudbaCheck.AutoSize = true;
-            this.PosudbaCheck.Location = new System.Drawing.Point(311, 106);
-            this.PosudbaCheck.Name = "PosudbaCheck";
-            this.PosudbaCheck.Size = new System.Drawing.Size(15, 14);
-            this.PosudbaCheck.TabIndex = 13;
-            this.PosudbaCheck.UseVisualStyleBackColor = true;
+            this.pretragaPosuditeljaKnjigeButton.Location = new System.Drawing.Point(614, 158);
+            this.pretragaPosuditeljaKnjigeButton.Name = "pretragaPosuditeljaKnjigeButton";
+            this.pretragaPosuditeljaKnjigeButton.Size = new System.Drawing.Size(75, 23);
+            this.pretragaPosuditeljaKnjigeButton.TabIndex = 28;
+            this.pretragaPosuditeljaKnjigeButton.Text = "Oboje";
+            this.pretragaPosuditeljaKnjigeButton.UseVisualStyleBackColor = true;
+            this.pretragaPosuditeljaKnjigeButton.Click += new System.EventHandler(this.pretragaPosuditeljaKnjigeButton_Click);
             // 
-            // VracanjeCheck
+            // pretragaLabel
             // 
-            this.VracanjeCheck.AutoSize = true;
-            this.VracanjeCheck.Location = new System.Drawing.Point(311, 149);
-            this.VracanjeCheck.Name = "VracanjeCheck";
-            this.VracanjeCheck.Size = new System.Drawing.Size(15, 14);
-            this.VracanjeCheck.TabIndex = 15;
-            this.VracanjeCheck.UseVisualStyleBackColor = true;
-            // 
-            // ISBNCheck
-            // 
-            this.ISBNCheck.AutoSize = true;
-            this.ISBNCheck.Location = new System.Drawing.Point(311, 63);
-            this.ISBNCheck.Name = "ISBNCheck";
-            this.ISBNCheck.Size = new System.Drawing.Size(15, 14);
-            this.ISBNCheck.TabIndex = 16;
-            this.ISBNCheck.UseVisualStyleBackColor = true;
+            this.pretragaLabel.AutoSize = true;
+            this.pretragaLabel.Location = new System.Drawing.Point(611, 33);
+            this.pretragaLabel.Name = "pretragaLabel";
+            this.pretragaLabel.Size = new System.Drawing.Size(50, 13);
+            this.pretragaLabel.TabIndex = 29;
+            this.pretragaLabel.Text = "Pretraga:";
             // 
             // Pretraga
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 275);
-            this.Controls.Add(this.ISBNCheck);
-            this.Controls.Add(this.VracanjeCheck);
-            this.Controls.Add(this.PosudbaCheck);
-            this.Controls.Add(this.OIBCheck);
-            this.Controls.Add(this.ZatvoriButun);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(708, 275);
+            this.Controls.Add(this.pretragaLabel);
+            this.Controls.Add(this.pretragaPosuditeljaKnjigeButton);
+            this.Controls.Add(this.pretragaKnjigeButton);
+            this.Controls.Add(this.pretragaKorisnikaButton);
+            this.Controls.Add(this.ISBNtextBox);
+            this.Controls.Add(this.ISBNLabel);
+            this.Controls.Add(this.AutortextBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NazivtextBox);
+            this.Controls.Add(this.Nazivlabel);
+            this.Controls.Add(this.OIBtextBox);
+            this.Controls.Add(this.OIBLabel);
+            this.Controls.Add(this.prezimeTextBox);
+            this.Controls.Add(this.prezimeLabel);
+            this.Controls.Add(this.imeTextBox);
+            this.Controls.Add(this.imeLabel);
             this.Controls.Add(this.PretragaRichTextbox);
-            this.Controls.Add(this.PosudbaPretraga);
-            this.Controls.Add(this.KnjigaPretraga);
-            this.Controls.Add(this.KorisnikPretraga);
             this.Controls.Add(this.PretragaButun);
-            this.Controls.Add(this.VracanjePretraga);
             this.Name = "Pretraga";
             this.Text = "Pretraga";
             this.ResumeLayout(false);
@@ -201,21 +238,23 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox VracanjePretraga;
         private System.Windows.Forms.Button PretragaButun;
-        private System.Windows.Forms.TextBox KorisnikPretraga;
-        private System.Windows.Forms.TextBox KnjigaPretraga;
-        private System.Windows.Forms.TextBox PosudbaPretraga;
         private System.Windows.Forms.RichTextBox PretragaRichTextbox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label imeLabel;
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.TextBox prezimeTextBox;
+        private System.Windows.Forms.Label prezimeLabel;
+        private System.Windows.Forms.TextBox OIBtextBox;
+        private System.Windows.Forms.Label OIBLabel;
+        private System.Windows.Forms.TextBox ISBNtextBox;
+        private System.Windows.Forms.Label ISBNLabel;
+        private System.Windows.Forms.TextBox AutortextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button ZatvoriButun;
-        private System.Windows.Forms.CheckBox OIBCheck;
-        private System.Windows.Forms.CheckBox PosudbaCheck;
-        private System.Windows.Forms.CheckBox VracanjeCheck;
-        private System.Windows.Forms.CheckBox ISBNCheck;
+        private System.Windows.Forms.TextBox NazivtextBox;
+        private System.Windows.Forms.Label Nazivlabel;
+        private System.Windows.Forms.Button pretragaKorisnikaButton;
+        private System.Windows.Forms.Button pretragaKnjigeButton;
+        private System.Windows.Forms.Button pretragaPosuditeljaKnjigeButton;
+        private System.Windows.Forms.Label pretragaLabel;
     }
 }
--- AplikacijaZaBiblioteku/Pretraga.cs
+++ AplikacijaZaBiblioteku/Pretraga.cs
@@ -22,69 +22,93 @@ namespace AplikacijaZaBiblioteku
         public Pretraga()
         {
             InitializeComponent();
+
         }
 
         private void PretragaButun_Click(object sender, EventArgs e)
         {
             try
             {
-                PretragaRichTextbox.Text = "";
-                var Knjige = XDocument.Load(pathKnjige);
-                var Posuditelji = XDocument.Load(pathPosuditelji);
-                var PosudbaVracanje = XDocument.Load(pathPosudbaVracanje);
-                if (OIBCheck.Checked)
-                {
-                    IEnumerable<string> OIBList = from s in Posuditelji.Descendants("Korisnik")
-                                                  where s.Element("OIB").Value == KorisnikPretraga.Text
-                                                  select "OIB: " + s.Element("OIB").Value +
-                                                       "\nIme: " + s.Element("Ime").Value +
-                                                       "\nPrezime: " + s.Element("Prezime").Value + "\n\n";
-                    foreach (string s in OIBList) { PretragaRichTextbox.Text += s; }
-                }
-                if (ISBNCheck.Checked)
-                {
-                    IEnumerable<string> ISBNList = from s in Knjige.Descendants("Knjiga")
-                                                   where s.Element("ISBN").Value == KnjigaPretraga.Text
-                                                   select "ISBN: " + s.Element("ISBN").Value +
-                                                          "\nNaziv: " + s.Element("Naziv").Value +
-                                                          "\nAutor: " + s.Element("Autor").Value +
-                                                          "\nKolicina: " + s.Element("Kolicina").Value + "\n\n";
-                    foreach (string s in ISBNList) { PretragaRichTextbox.Text += s; }
-                }
-                if (VracanjeCheck.Checked)
-                {
-                    IEnumerable<string> VRACANJEList = from s in PosudbaVracanje.Descendants("Vracanje")
-                                                       where s.Element("Knjiga").Value == VracanjePretraga.Text
-                                                       select "Datum Vracanja: " + s.Element("VracanjeDatum").Value +
-                                                              "\nOIB: " + s.Element("Korisnik").Value +
-                                                              "\nISBN: " + s.Element("Knjiga").Value + "\n\n";
-                    foreach (string s in VRACANJEList) { PretragaRichTextbox.Text += s; }
-                }
-                if (PosudbaCheck.Checked)
-                {
-                    IEnumerable<string> POSUDBAList = from s in PosudbaVracanje.Descendants("Posudivanje")
-                                                      where s.Element("Knjiga").Value == PosudbaPretraga.Text
-                                                      select "Datum Posudbe: " + s.Element("PosudivanjeDatum").Value +
-                                                             "\nOIB: " + s.Element("Korisnik").Value +
-                                                             "\nISBN: " + s.Element("Knjiga").Value + "\n\n";
-                    foreach (string s in POSUDBAList) { PretragaRichTextbox.Text += s; }
-                }
+                //PretragaRichTextbox.Text = "";
+                //var Knjige = XDocument.Load(pathKnjige);
+                //var Posuditelji = XDocument.Load(pathPosuditelji);
+                //var PosudbaVracanje = XDocument.Load(pathPosudbaVracanje);
+                //if (OIBCheck.Checked)
+                //{
+                //    IEnumerable<string> OIBList = from s in Posuditelji.Descendants("Korisnik")
+                //                                  where s.Element("OIB").Value == KorisnikPretraga.Text
+                //                                  select "OIB: " + s.Element("OIB").Value +
+                //                                       "\nIme: " + s.Element("Ime").Value +
+                //                                       "\nPrezime: " + s.Element("Prezime").Value + "\n\n";
+                //    foreach (string s in OIBList) { PretragaRichTextbox.Text += s; }
+                //}
+                //if (ISBNCheck.Checked)
+                //{
+                //    IEnumerable<string> ISBNList = from s in Knjige.Descendants("Knjiga")
+                //                                   where s.Element("ISBN").Value == KnjigaPretraga.Text
+                //                                   select "ISBN: " + s.Element("ISBN").Value +
+                //                                          "\nNaziv: " + s.Element("Naziv").Value +
+                //                                          "\nAutor: " + s.Element("Autor").Value +
+                //                                          "\nKolicina: " + s.Element("Kolicina").Value + "\n\n";
+                //    foreach (string s in ISBNList) { PretragaRichTextbox.Text += s; }
+                //}
+                //if (VracanjeCheck.Checked)
+                //{
+                //    IEnumerable<string> VRACANJEList = from s in PosudbaVracanje.Descendants("Vracanje")
+                //                                       where s.Element("Knjiga").Value == VracanjePretraga.Text
+                //                                       select "Datum Vracanja: " + s.Element("VracanjeDatum").Value +
+                //                                              "\nOIB: " + s.Element("Korisnik").Value +
+                //                                              "\nISBN: " + s.Element("Knjiga").Value + "\n\n";
+                //    foreach (string s in VRACANJEList) { PretragaRichTextbox.Text += s; }
+                //}
+                //if (PosudbaCheck.Checked)
+                //{
+                //    IEnumerable<string> POSUDBAList = from s in PosudbaVracanje.Descendants("Posudivanje")
+                //                                      where s.Element("Knjiga").Value == PosudbaPretraga.Text
+                //                                      select "Datum Posudbe: " + s.Element("PosudivanjeDatum").Value +
+                //                                             "\nOIB: " + s.Element("Korisnik").Value +
+                //                                             "\nISBN: " + s.Element("Knjiga").Value + "\n\n";
+                //    foreach (string s in POSUDBAList) { PretragaRichTextbox.Text += s; }
+                //}
             }
             catch (Exception except)
             {
-
                 if (except.Message.Contains("Knjig")) MessageBox.Show("Niste unijeli ni jednu knjigu", "Pogreska", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 if (except.Message.Contains("Posuditelj")) MessageBox.Show("Niste unijeli ni jednog posuditelja!", "Pogreska", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 if (except.Message.Contains("PosudbaVracanje")) MessageBox.Show("Niste unijeli ni jednu posudbu!", "Pogreska", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                
-
             }
         }
 
-        private void ZatvoriButun_Click(object sender, EventArgs e)
+        private void changeState(bool state)
         {
-            this.Close();
+            imeTextBox.Enabled = state;
+            prezimeTextBox.Enabled = state;
+            OIBtextBox.Enabled = state;
+
+            NazivtextBox.Enabled = !state;
+            AutortextBox.Enabled = !state;
+            ISBNtextBox.Enabled = !state;
+        }
+
+        private void pretragaKorisnikaButton_Click(object sender, EventArgs e)
+        {
+            changeState(true);
+        }
+
+        private void pretragaKnjigeButton_Click(object sender, EventArgs e)
+        {
+            changeState(false);
+        }
+
+        private void pretragaPosuditeljaKnjigeButton_Click(object sender, EventArgs e)
+        {
+            imeTextBox.Enabled = true;
+            prezimeTextBox.Enabled = true;
+            OIBtextBox.Enabled = true;
+
+            NazivtextBox.Enabled = true;
+            AutortextBox.Enabled = true;
+            ISBNtextBox.Enabled = true;
         }
     }
 }
